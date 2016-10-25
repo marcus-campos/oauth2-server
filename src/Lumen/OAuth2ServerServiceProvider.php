f19b83e0565d7dd522309225d8b12544fed03002
Marcus Campos <marcus_ultimate@hotmail.com>
2016-10-25 17:55:50 -0200

    Initial Commit

---
+++ src/Lumen/OAuth2ServerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+/*
+ * This file is part of OAuth 2.0 Laravel.
+ *
+ * (c) Marcus Campos <marcus_vinícius_campos@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace MarcusCampos\OAuth2Server\Lumen;
+
+use MarcusCampos\OAuth2Server\OAuth2ServerServiceProvider as ServiceProvider;
+
+/**
+ * This is the Lumen oauth server service provider class.
+ *
+ * @deprecated since version 5.1. Use the base OAuth2ServerServiceProvider instead.
+ *
+ * @author Luca Degasperi <packages@lucadegasperi.com>
+ */
+class OAuth2ServerServiceProvider extends ServiceProvider
+{
+    //
+}
